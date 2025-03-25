8b8b425dd7bcc15a316f9acd149960a55c2b4f79
nextples <m.gankeev@g.nsu.ru>
2025-03-26 01:05:13 +0700

    init

---
+++ ms_users/src/test/java/ru/nsu/geoapp/ms_users/MsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.geoapp.ms_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
