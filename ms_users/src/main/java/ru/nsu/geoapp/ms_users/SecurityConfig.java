190e6632b715e5a2bcc28e72c5aab09c2e539690
Just <grivten@yandex.ru>
2025-05-10 18:14:15 +0700

    changed matcher for secured urls

--- ms_users/src/main/java/ru/nsu/geoapp/ms_users/SecurityConfig.java
+++ ms_users/src/main/java/ru/nsu/geoapp/ms_users/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/google").permitAll()
+                        .requestMatchers("/auth/google").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
