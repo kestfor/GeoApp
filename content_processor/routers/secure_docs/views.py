88b071d19265f9e446ee7d62c2f3472d0cc464cb
Антон Адонин <68176962+AntonAdonin@users.noreply.github.com>
2025-05-20 22:17:06 +0700

    fix docs + ports

--- content_processor/routers/secure_docs/views.py
+++ content_processor/routers/secure_docs/views.py
@@ -1,6 +1,7 @@
 # Define your module's routes here
 import secrets
 
+from core.config import settings
 from fastapi import APIRouter, Depends, HTTPException, FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
 from fastapi.openapi.utils import get_openapi
@@ -8,8 +9,6 @@ from fastapi.security import HTTPBasicCredentials, HTTPBasic
 from starlette import status
 from starlette.requests import Request
 from starlette.responses import HTMLResponse, JSONResponse
-
-from core.config import settings
 
 router = APIRouter(tags=["docs"])
 security = HTTPBasic()
@@ -28,24 +27,25 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)) 
 
 
 def get_docs_auth_dep():
-    # TODO: not work
     if settings.secure_docs.IS_ENABLED:
         return Depends(get_current_username)
     return None  # Не требует авторизации
 
 
 @router.get("/docs", response_class=HTMLResponse)
-async def get_docs(username: str = Depends(get_docs_auth_dep)) -> HTMLResponse:
-    return get_swagger_ui_html(openapi_url="/api/openapi.json", title="docs")
+async def get_docs(request: Request, username: str = get_docs_auth_dep()) -> HTMLResponse:
+    base_url = request.base_url
+    return get_swagger_ui_html(openapi_url=f"{base_url.path[:-4]}/api/openapi.json", title="docs")
 
 
 @router.get("/redoc", response_class=HTMLResponse)
-async def get_redoc(username: str = Depends(get_docs_auth_dep)) -> HTMLResponse:
-    return get_redoc_html(openapi_url="/api/openapi.json", title="redoc")
+async def get_redoc(request: Request, username: str = get_docs_auth_dep()) -> HTMLResponse:
+    base_url = request.base_url
+    return get_redoc_html(openapi_url=f"{base_url.path[:-4]}/api/openapi.json", title="redoc")
 
 
 @router.get("/api/openapi.json", response_class=JSONResponse)
-async def get_openapi_json(request: Request, username: str = Depends(get_docs_auth_dep)) -> JSONResponse:
+async def get_openapi_json(request: Request, username: str = get_docs_auth_dep()) -> JSONResponse:
     app: FastAPI = request.app
     openapi_schema = get_openapi(
         title=app.title,
