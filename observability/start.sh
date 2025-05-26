d1fcceaa60ac1a50235febb8080cb11ce0651df0
Антон Адонин <68176962+AntonAdonin@users.noreply.github.com>
2025-05-26 23:14:33 +0700

    remove volume driver

--- observability/start.sh
+++ observability/start.sh
@@ -1,6 +1,2 @@
-mkdir -p ./docker_volumes/grafanadata \
-         ./docker_volumes/prometheusdata \
-         ./docker_volumes/tempodata \
-         ./docker_volumes/lokidata
 docker network create "shared-network"
 docker compose up -d --build
