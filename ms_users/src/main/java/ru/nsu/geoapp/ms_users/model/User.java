89aa61ef287519093da6af661359b206ef2ec8ad
kestfor <anzhinilya2@gmail.com>
2025-06-04 15:16:08 +0700

    fixed scheme

--- ms_users/src/main/java/ru/nsu/geoapp/ms_users/model/User.java
+++ ms_users/src/main/java/ru/nsu/geoapp/ms_users/model/User.java
@@ -26,7 +26,7 @@ public class User {
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "last_name")
     private String lastName;
 
     @Column(name = "picture_url")
